b794369fa0acb6722e38a28126d59d595190468c
Endika Gutiérrez Salas <me@endika.net>
2013-12-13 22:20:32 +0100

    Added Test target.

---
+++ Log4Cocoa Tests/Log4Cocoa_Tests.m
@@ -0,0 +1,34 @@
+//
+//  Log4Cocoa_Tests.m
+//  Log4Cocoa Tests
+//
+//  Created by Endika Gutiérrez Salas on 12/13/13.
+//  Copyright (c) 2013 EPD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Log4Cocoa_Tests : XCTestCase
+
+@end
+
+@implementation Log4Cocoa_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
